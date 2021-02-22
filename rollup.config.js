c3b8367386365bb4cf219878ef8fe75c84560750
evgeniia <74870106+evgeniia-pe-ace@users.noreply.github.com>
2021-02-22 15:59:40 +0900

    first
    first

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from 'rollup-plugin-node-resolve'
+import cjs from 'rollup-plugin-commonjs'
+import vue from 'rollup-plugin-vue'
+import { terser } from 'rollup-plugin-terser'
+
+import pkg from './package.json'
+
+const banner = `// vue-zoomer v${ pkg.version } - Jarvis Niu
+// https://github.com/jarvisniu/vue-zoomer\n`
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      banner,
+      file: 'dist/vue-zoomer.esm.js',
+      format: 'esm',
+    },
+    {
+      banner,
+      file: 'dist/vue-zoomer.js',
+      format: 'umd',
+      name: 'VueZoomer',
+    },
+    {
+      banner,
+      file: 'dist/vue-zoomer.min.js',
+      format: 'umd',
+      name: 'VueZoomer',
+    },
+  ],
+  plugins: [
+    resolve(),
+    cjs(),
+    vue({
+      needMap: false,
+    }),
+    terser({
+      include: /^.+\.min\.js$/,
+    }),
+  ],
+}
